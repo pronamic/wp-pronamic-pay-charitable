ddedf79de012056b0aba480b7bbcdccc9425a7b2
Reüel van der Steege <reuel@pronamic.nl>
2021-01-18 16:05:03 +0100

    Happy 2021.

--- src/CreditCardGateway.php
+++ src/CreditCardGateway.php
@@ -8,7 +8,7 @@ use Pronamic\WordPress\Pay\Core\PaymentMethods;
 /**
  * Title: Charitable Credit Card gateway
  * Description:
- * Copyright: 2005-2020 Pronamic
+ * Copyright: 2005-2021 Pronamic
  * Company: Pronamic
  *
  * @author  Remco Tolsma
--- tests/src/CharitableTest.php
+++ tests/src/CharitableTest.php
@@ -7,7 +7,7 @@ use PHPUnit_Framework_TestCase;
 /**
  * Title: WordPress pay Charitable test
  * Description:
- * Copyright: 2005-2020 Pronamic
+ * Copyright: 2005-2021 Pronamic
  * Company: Pronamic
  *
  * @author  Remco Tolsma
