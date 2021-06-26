895de9c9c4872f2a739b7cf0d7ad50b2d0109e43
Reüel van der Steege <reuel@pronamic.nl>
2021-06-26 23:46:00 +0200

    Add support for PayPal payment method.

---
+++ src/PaypalGateway.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Pronamic\WordPress\Pay\Extensions\Charitable;
+
+use Charitable_Donation_Processor;
+use Pronamic\WordPress\Pay\Core\PaymentMethods;
+
+/**
+ * Title: Charitable PayPal gateway
+ * Description:
+ * Copyright: 2005-2021 Pronamic
+ * Company: Pronamic
+ *
+ * @author  Reüel van der Steege
+ * @version 2.3.0
+ * @since   2.3.0
+ */
+class PayPalGateway extends Gateway {
+	/**
+	 * The unique ID of this payment gateway
+	 *
+	 * @var string
+	 */
+	const ID = 'pronamic_pay_paypal';
+
+	/**
+	 * Payment method.
+	 *
+	 * @var string
+	 */
+	protected $payment_method = PaymentMethods::PAYPAL;
+
+	/**
+	 * Process donation.
+	 *
+	 * @since   2.3.0
+	 *
+	 * @param bool|array                    $return      Return.
+	 * @param int                           $donation_id Donation ID.
+	 * @param Charitable_Donation_Processor $processor   Charitable donation processor.
+	 * @return bool|array
+	 */
+	public static function process_donation( $return, $donation_id, $processor ) {
+		return self::pronamic_process_donation( $return, $donation_id, $processor, new self() );
+	}
+
+	/**
+	 * Returns the current gateway's ID.
+	 *
+	 * @return string
+	 * @since  2.3.0
+	 */
+	public static function get_gateway_id() {
+		return self::ID;
+	}
+}
