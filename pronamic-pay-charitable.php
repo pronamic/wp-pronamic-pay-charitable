d7fe5f238f88d8003dc0609906f2d62451864a5f
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2023-01-31 13:49:41 +0100

    v4.3.1

--- pronamic-pay-charitable.php
+++ pronamic-pay-charitable.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://www.pronamic.eu/plugins/pronamic-pay-charitable/
  * Description: Extend the Pronamic Pay plugin with Charitable support to receive payments through a variety of payment providers.
  *
- * Version: 4.3.0
+ * Version: 4.3.1
  * Requires at least: 4.7
  * Requires PHP: 8.0
  *
