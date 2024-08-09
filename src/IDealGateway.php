1398f57fa132f8f3634cf719f8188de149f0c1db
Reüel van der Steege <reuel@pronamic.nl>
2024-08-09 16:29:44 +0200

    Happy 2024.

--- src/IDealGateway.php
+++ src/IDealGateway.php
@@ -3,7 +3,7 @@
  * IDEAL gateway.
  *
  * @author    Pronamic <info@pronamic.eu>
- * @copyright 2005-2023 Pronamic
+ * @copyright 2005-2024 Pronamic
  * @license   GPL-3.0-or-later
  * @package   Pronamic\WordPress\Pay
  */
@@ -20,7 +20,7 @@ use Pronamic\WordPress\Pay\Plugin;
 /**
  * Title: Charitable iDEAL gateway
  * Description:
- * Copyright: 2005-2023 Pronamic
+ * Copyright: 2005-2024 Pronamic
  * Company: Pronamic
  *
  * @author  Remco Tolsma
